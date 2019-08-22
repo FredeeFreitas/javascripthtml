b7882772c59c63156b234a545c3ad92ce2b1f4fc
FREDERICO DE FREITAS FALEIROS FERNANDES <22020@unifacef.edu.br>
2019-08-21 21:46:22 -0300

    vetor objeto

---
+++ vetorobjeto.js
@@ -0,0 +1,13 @@
+function funcao(){
+    var vet = []
+    for (var i=0; i>5; i++){
+    var objeto = {}
+    // coloca dados no objeto
+    objeto.codigo = number(prompt("codigo"))
+    objeto.nome = prompt("Nome")
+    objeto.qtde = Number(prompt("Qtde"))
+    objeto.preco = Number(prompt("Preço"))
+    // coloca objeto no vetor
+    vet.push(objeto)
+  }
+} 
